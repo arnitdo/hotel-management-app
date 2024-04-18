cb485026f9411df66fe7d688decebea3f13c9ac9
arnitdo <arnav.nitin.deo@gmail.com>
2024-04-19 03:31:55 +0530

    Add entire app :)

---
+++ src/app/auth/logout/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
+
+export default function Logout(){
+	const router = useRouter()
+
+	useEffect(() => {
+		localStorage.removeItem("app-data")
+		router.push("/")
+	}, []);
+
+	return (
+		<>
+
+		</>
+	)
+}
---
+++ src/util/Scratch.tsx
@@ -0,0 +1,12 @@
+export default async function Component(){
+	return (
+		<main className="flex min-h-screen p-32 flex-col items-center justify-center gold-bg">
+			<div className={"bg-white flex flex-col gap-8 p-16 border rounded-2xl shadow-2xl z-10"} >
+				<h1 className={"font-bold text-4xl text-amber-600"}>
+					Sign up
+				</h1>
+				<hr />
+			</div>
+		</main>
+	);
+}
